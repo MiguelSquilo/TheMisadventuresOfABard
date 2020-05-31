fe7356bd108ea2f5578bc2fd681bcad94b1a36fa
MiguelSquilo <vfcoolmiklas@gmail.com>
2020-05-31 15:10:52 +0100

    First Commit
    Trabalho Inicial do Silva

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Advance to the next Scene in the order (Game or Level0)
+    }
+
+    public void QuitGame(){
+    	Application.Quit();
+    }
+}
