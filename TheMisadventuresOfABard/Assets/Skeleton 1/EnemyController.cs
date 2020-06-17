f4d088e3a452d9b63c2f6681f00decc6d0e2ed6c
ConstantinCioacaa <64656126+ConstantinCioacaa@users.noreply.github.com>
2020-06-17 01:54:15 +0100

    tem vida e o enemy ataca

---
+++ TheMisadventuresOfABard/Assets/Skeleton 1/EnemyController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    private Animator myAnim;
+    private Transform target;
+    [SerializeField]
+    private float speed = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnim = GetComponent<Animator>();
+        target = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+    }
+
+    public void FollowPlayer()
+    {
+        myAnim.SetBool("isMoving", true);
+        myAnim.SetFloat("MoveX", (target.position.x - transform.position.x));
+        myAnim.SetFloat("MoveY", (target.position.y - transform.position.y));
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
